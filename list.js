c0364185ed6193bb7fcc9329ba50dc707297b59e
OgucheDivine <oguchedivine04@gmail.com>
2024-10-17 09:59:53 +0100

    list.js

--- list.js
+++ list.js
@@ -23,7 +23,7 @@ function create(){
     let input=document.querySelector('#input')
     if (input.value !='  ' &&input.value !=' ' && input.value !=''){
     let ele=document.createElement('p')
-    ele.innerHTML=`<img src="check.svg" onclick="checker(this)" height=15.8% width=15.8% > ${input.value}<img src="bin.svg" height="15.9%" width="15.63%" onclick="this.parentNode.remove()">`
+    ele.innerHTML=`<img src="checked.svg" onclick="checker(this)" height=15.8% width=15.8% > ${input.value}<img src="bin.svg" height="15.9%" width="15.63%" onclick="this.parentNode.remove()">`
     list.appendChild(ele)
     input.value=''}
 }
